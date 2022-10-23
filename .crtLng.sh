2344b40df2db1240f11b2b72f91eed3b8f5688d3
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2022-10-23 21:09:03 +0200

    Added translations for the rollout-menu, fixed error that earlier translations were removed

--- .crtLng.sh
+++ .crtLng.sh
@@ -13,9 +13,9 @@ for F in $(ls _build/gettext/* | grep -v -f .exclude | cut -d/ -f3); do
 #  grep ' = ' _locale/pot/${F} | sed 's/msgid //' | sed 's/msgstr //' | grep '[\\\\|\|]' | uniq -u | sed -e "s/^/${F}: /"
 done
 sed -i "/Language:.*/d" _locale/pot/*.pot
-for F in $(find _locale -name *.po); do
-   msgmerge -q _locale/pot/$(echo ${F} | cut -d"/" -f4-)t ${F} -o ${F}
-done   
+#for F in $(find _locale -name *.po); do
+#   msgmerge -q _locale/pot/$(echo ${F} | cut -d"/" -f4-)t ${F} -o tmp.po && mv tmp.po ${F}
+#done   
 #for F in $(find _locale -name *.po); do
 #   FS=$(echo ${F} | cut -d"/" -f4-);
 #   if [ ! -e "_locale/${FS}t" ]; then
